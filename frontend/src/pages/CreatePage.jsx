f56dfe016ac3dcc48b3e0251db22bac825ef3eec
AleksandarI01 <128722198+AleksandarI01@users.noreply.github.com>
2025-04-04 18:01:16 +0200

    UI of Create Page

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,6 +1,55 @@
+import { Box, Button, Container, Heading, Input, useColorModeValue, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+
 const CreatePage = () => {
+    const [newItem, setNewItem] = useState({
+        name: "",
+        price: "",
+        image: ""
+    });
+
+    const handleAddItem = () => {
+        console.log("new Item", newItem)
+    };
+
     return (
-        <div>CreatePage</div>
+        <Container maxW={"container.sm"}>
+            <VStack spacing={8}>
+                <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+                    Add a new Item to your Wishlist
+                </Heading>
+
+                <Box w={"full"} bg={useColorModeValue("white", "gray.800")}
+                    p={6} rounded={"lg"} shadow={"md"}
+                >
+                    <VStack spacing={4}>
+                        <Input
+                            placeholder='Item Name'
+                            name='name'
+                            value={newItem.name}
+                            onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
+                        />
+                        <Input
+                            placeholder='Price'
+                            name='price'
+                            type='number'
+                            value={newItem.price}
+                            onChange={(e) => setNewItem({ ...newItem, price: e.target.value })}
+                        />
+                        <Input
+                            placeholder='Image URL'
+                            name='image'
+                            value={newItem.image}
+                            onChange={(e) => setNewItem({ ...newItem, image: e.target.value })}
+                        />
+
+                        <Button colorShema='blue' onClick={handleAddItem} w='full'>
+                            Add Item
+                        </Button>
+                    </VStack>
+                </Box>
+            </VStack>
+        </Container >
     )
 }
 
